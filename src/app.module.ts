21242edc411a9dc8e7a20da388acf10c9dc46acb
Ojietohamen Samuel <seekersoftec@gmail.com>
2024-05-30 07:36:20 +0100

    add database and users modules to main app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { DatabaseModule } from '@/database/database.module';
+import { DatabaseService } from '@/database/database.service';
+import { UsersModule } from './users/users.module';
 // import { AppController } from './app.controller';
 // import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
-import { DatabaseService } from './database/database.service';
-
 // @Module({
 //   imports: [],
 //   controllers: [AppController],
@@ -18,8 +18,9 @@ import { DatabaseService } from './database/database.service';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true,
     }),
-    // UsersModule,
+    UsersModule,
   ],
   providers: [DatabaseService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,31 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
+async function bootstrap(configService: ConfigService) {
+  const logger = new Logger('Response');
   const app = await NestFactory.create(AppModule);
+
+  // const swaggerConfig = new DocumentBuilder()
+  //   .setTitle('Backend')
+  //   .setDescription('The Backend API description')
+  //   .setVersion('0.1')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, swaggerConfig);
+  // SwaggerModule.setup('api', app, document);
+
   app.enableCors();
-  app.setGlobalPrefix('api/v1');
+  // app.setGlobalPrefix('api/v1');
+
+  const port = configService.get('PORT');
+
   await app.listen(process.env.PORT);
 
   logger.log(`Application listening on port ${port}`);
 }
-bootstrap();
+
+bootstrap(new ConfigService());
+
+// https://gemini.google.com/app/46e38a303f222efb
